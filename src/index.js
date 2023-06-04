dc878cef1e8c85becd58020e302fa932b58c9c06
Mike Vercoelen <mike.vercoelen@gmail.com>
2023-06-04 09:23:23 +0200

    Base

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+const puppeteer = require('puppeteer')
+
+const WOODHOUSE_URL = 'http://woodhouse-pr1691.nestiostaging.com/6/welcome'
+
 async function main () {
-  console.log('Hello World!')
+  const browser = await puppeteer.launch({
+    headless: 'new'
+  })
+
+  const page = await browser.newPage()
+
+  await page.goto(WOODHOUSE_URL)
+
+  await page.setViewport({ width: 1920, height: 1080 })
 }
 
-void main()
+main()
